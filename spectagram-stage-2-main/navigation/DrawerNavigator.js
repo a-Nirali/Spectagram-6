8512f3245c9cdfa17932d46080bc2279d0bc3a90
a-Nirali <74716227+a-Nirali@users.noreply.github.com>
2021-07-17 09:58:46 +0530

    Update DrawerNavigator.js

--- spectagram-stage-2-main/navigation/DrawerNavigator.js
+++ spectagram-stage-2-main/navigation/DrawerNavigator.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import TabNavigator from "./TabNavigator";
+import StackNavigator from "./StackNavigator";
 import Profile from "../screens/Profile";
+import Logout from "../screens/Logout";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
     return (
         <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={TabNavigator} />
+            <Drawer.Screen name="Home" component={StackNavigator} />
             <Drawer.Screen name="Profile" component={Profile} />
+            <Drawer.Screen name="Logout" component={Logout} />
         </Drawer.Navigator>
     );
 };
